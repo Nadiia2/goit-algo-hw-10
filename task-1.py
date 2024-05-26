71085940424dd1db3f7c03fe8026bcbdb75987bf
Nadiia2 <studenikina.nadya@gmail.com>
2024-05-26 13:34:48 +0200

    Update task-1.py

--- task-1.py
+++ task-1.py
@@ -3,18 +3,18 @@ from pulp import *
 lemonade = LpVariable("Lemonade_units", lowBound=0, cat='Integer')
 fruit_juice = LpVariable("Fruit_juice_units", lowBound=0, cat='Integer')
 
-prob = LpProblem("Maximize_production", LpMaximize)
+model = LpProblem("Maximize_production", LpMaximize)
 
-prob += lemonade + fruit_juice, "Total_production"
+model += lemonade + fruit_juice, "Total_Production"
+# Обмеження на ресурси
+model += 2 * lemonade + 1 * fruit_juice <= 100, "Water_Constraint"
+model += 1 * lemonade <= 50, "Sugar_Constraint"
+model += 1 * lemonade <= 30, "Lemon_Juice_Constraint"
+model += 2 * fruit_juice <= 40, "Fruit_Puree_Constraint"
 
-prob += 2 * lemonade + fruit_juice <= 100, "Water_constraint"
-prob += lemonade <= 50, "Sugar_constraint"
-prob += lemonade <= 30, "Lemon_juice_constraint"
-prob += 2 * fruit_juice + lemonade <= 40, "Fruit_puree_constraint"
+model.solve()
 
-prob.solve()
-
-print("Status:", LpStatus[prob.status])
-print("Total production:", value(prob.objective))
+print("Status:", LpStatus[model.status])
+print("Total production:", value(model.objective))
 print("Lemonade units:", value(lemonade))
 print("Fruit juice units:", value(fruit_juice))
